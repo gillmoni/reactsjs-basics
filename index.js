dd7034ef62a622c811c17e5f539d5c0b1b834031
Moni <gill.moninder@gmail.com>
2023-04-13 20:47:14 -0400

    Adding data using varilable and passing values as JSX

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-ReactDOM.render(
-<h1>Hello, React! from index.js
-    <div id="ul-list">
-        <h6>
-            <ul>
-                <li>First element</li>
-                <li>Second element</li>
-                <li>Third element</li>
-            </ul>
-        </h6>
-        <div><MainComponent/></div>
-        <div><Navbar /></div>
-    </div>
-</h1> , document.getElementById("root"))
+const h1 = document.createElement("h1")
+h1.textContent = "Hello World"
+h1.className = "header"
+console.log(h1)
+
+//Simple JSX syntax
+const element_1 = <h1 className="header">This is JSX, syntax mix and passing via variable</h1>
+
+//Declare multiple but wrap in parenthesis
+const element_2 = (
+                    <h1 className="header">Hello, React! from index.js
+                        <div id="ul-list">
+                            <h6>
+                                <ul>
+                                    <li>First element</li>
+                                    <li>Second element</li>
+                                    <li>Third element</li>
+                                </ul>
+                            </h6>
+                            <div><MainComponent/></div>
+                            <div><Navbar /></div>
+                        </div>
+                    </h1> 
+                    )
+
+//passing variable
+let element_4 = "New Stuff"
+
+//passing object
+let element_5 = element_1 + element_2
+
+console.log(`let me show you the value ${element_5}`)
+
+//Use all those variable in render function
+ReactDOM.render(element_2
+, document.getElementById("root"))
 
 
 function Navbar() {
@@ -22,34 +44,7 @@ function Navbar() {
                 <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" href="#">Disabled</a>
-                </li>
-                </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
+ 
         </nav>
     )
 }
@@ -57,8 +52,8 @@ function Navbar() {
 
 function MainComponent() {
     return (
-        <h3 className="custom-element" id="custom-element">
+        <h5 className="custom-element" id="custom-element">
         <p>Just adding one custom element, nothing else.</p>
-        </h3>
+        </h5>
     )
 }

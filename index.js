8d0b46b5285edbca8f7125a22996a2c5ce0e6dbf
Moni <gill.moninder@gmail.com>
2023-04-13 20:14:58 -0400

    Add a UL element to React root ID

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-ReactDOM.render(<h1>Hello, React! from index.js</h1>, document.getElementById("root"))
+ReactDOM.render(
+<h1>Hello, React! from index.js
+    <div id="ul-list">
+        <h6>
+            <ul>
+                <li>First element</li>
+                <li>Second element</li>
+                <li>Third element</li>
+            </ul>
+        </h6>
+    </div>
+</h1> , document.getElementById("root"))

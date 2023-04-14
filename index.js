4694fffbe49e29ab3c266cd33d0e1de99285388d
Moni <gill.moninder@gmail.com>
2023-04-13 21:51:01 -0400

    1.16 Custom component in react, part2

--- index.js
+++ index.js
@@ -52,11 +52,30 @@ function MainComponent() {
     )
 }
 
+/**
+Challenge: 
+
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+ */
 
 //This is getting custom component to work in React
-function CustomComponent() {
+function Page() {
     return (
         <div>
+            <header>
+                <nav>
+                    <img src="./react-logo.png" width="40px"></img>
+                </nav>
+
+            </header>
             <h1>Reasons to learn react.</h1>
                 <ol>
                     <li>It's a popular library, so I'll be 
@@ -64,6 +83,11 @@ function CustomComponent() {
                     <li>I'm more likely to get a job as a developer
                     if I know React</li>
                 </ol>
+                <footer>
+                    <small><small>
+                        © 2023 YaYa development. All rights reserved.
+                    </small></small>
+                </footer>
         </div>
     )
 }
@@ -71,5 +95,5 @@ function CustomComponent() {
 //Use all those variable in render function
 //And wrap in angular brackets
 ReactDOM.render(
-    <CustomComponent />
+    <Page />
     , document.getElementById("root"))
